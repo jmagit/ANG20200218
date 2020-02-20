4d22d3d4b0bb3fe566366c058f6745af5c1ea89e
Javier <jmartin@grupoloyal.com>
2020-02-20 21:45:06 +0100

    Core

---
+++ src/my-core/my-core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PIPES_CADENAS } from './pipes/cadenas.pipe';
+import { MySizerComponent } from './components/my-sizer.component';
+import { PIPES_NUMERICOS } from './pipes/numericos.pipe';
+import { MIS_VALIDADORES } from './directives/validadores.directive';
+
+
+
+@NgModule({
+  declarations: [
+    PIPES_CADENAS, MySizerComponent, PIPES_NUMERICOS, MIS_VALIDADORES,
+  ],
+  exports: [
+    PIPES_CADENAS, MySizerComponent, PIPES_NUMERICOS, MIS_VALIDADORES,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+
+export class MyCoreModule { }
